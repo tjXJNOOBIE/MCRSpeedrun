49f2e61bd6444b2dfcb0b6c22d78597689d857f5
TJ <tjxjnoobie@gmail.com>
2025-09-27 15:13:55 -0700

    Added Spring Boot

---
+++ Website/src/main/java/com/tjxjnoobie/website/WebsiteApplication.java
@@ -0,0 +1,30 @@
+package com.tjxjnoobie.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class WebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteApplication.class, args);
+    }
+
+
+
+    @RestController
+    @RequestMapping("/api")
+    @CrossOrigin(origins = "http://localhost:5173") // allow React dev server
+
+    public static class HelloController {
+
+        @GetMapping("/test")
+        public String hello() {
+        return "Spring Boot is working!";
+    }
+    }
+}
