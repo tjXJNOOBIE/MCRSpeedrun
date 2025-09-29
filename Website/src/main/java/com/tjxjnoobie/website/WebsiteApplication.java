06db151530d8175587332a3df6750191b8af3a87
TJ <tjxjnoobie@gmail.com>
2025-09-29 07:27:19 -0700

    Updated pom.xml's, Added mvn extensions.xml & jgitver.config.xml

--- Website/src/main/java/com/tjxjnoobie/website/WebsiteApplication.java
+++ Website/src/main/java/com/tjxjnoobie/website/WebsiteApplication.java
@@ -2,12 +2,13 @@ package com.tjxjnoobie.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class WebsiteApplication {
 
     public static void main(String[] args) {
