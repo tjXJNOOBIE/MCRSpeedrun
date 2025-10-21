69e2e38e7db1b228db55b8e90af1c1c4b540d0d1
TJ <tjxjnoobie@gmail.com>
2025-10-21 04:19:18 -0700

    Refactor: Removed remaining DI method
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- API/src/main/java/com/tjxjnoobie/api/interfaces/IContext.java
+++ API/src/main/java/com/tjxjnoobie/api/interfaces/IContext.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 /**
  * Base interface for context management providing dependency injection capabilities
+ *
  * @param <T> The type of context implementation
  */
 public interface IContext<T> {
@@ -18,25 +19,19 @@ public interface IContext<T> {
      */
 
     T getContext();
+
     /**
      * Gets the dependency map for direct access
+     *
      * @return HashMap containing all registered dependencies
      */
     DependencyMap getDependencyMap();
 
 
     /**
-     * Checks if an object has any @Inject annotated fields
-     * @param obj The object to check
-     * @return true if the object has injectable fields, false otherwise
-     */
-    boolean hasInjectableFields(Object obj);
-    
-
-
-    /**
      * Returns a list of all registered context instances.
+     *
      * @return List of all IContext instances, never null.
      */
-     List<IContext<?>> getAllContexts();
+    List<IContext<?>> getAllContexts();
 }
