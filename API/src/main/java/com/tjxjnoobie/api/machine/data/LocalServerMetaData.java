cab62c59404520b7c18b07f60154f55e0c6f1a86
TJ <tjxjnoobie@gmail.com>
2025-10-25 01:14:13 -0700

    Add: `minecraftInGamePrefix` and `minecraftStaffInGamePrefix` fields to `LocalServerMetaData` class with corresponding getter methods and updated `setServerID` method
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- API/src/main/java/com/tjxjnoobie/api/machine/data/LocalServerMetaData.java
+++ API/src/main/java/com/tjxjnoobie/api/machine/data/LocalServerMetaData.java
@@ -6,20 +6,33 @@ public class LocalServerMetaData implements ILocalServerMetaData {
 
     public String serverID = "DEV";
     public String gameID;
+    public String minecraftInGamePrefix = "";
+    public String minecraftStaffInGamePrefix = "";
+
 
     @Override
     public String getServerID() {
         return serverID;
     }
     @Override
-    public void setServerID(String serverID) {
-        this.serverID = serverID;
-
+    public String getMinecraftInGamePrefix(){
+        return minecraftInGamePrefix;
+    }
+    @Override
+    public String getMinecraftStaffInGamePrefix(){
+        return minecraftStaffInGamePrefix;
     }
     @Override
     public String getGameID() {
         return gameID;
     }
+
+    @Override
+    public void setServerID(String serverID) {
+        this.serverID = serverID;
+
+    }
+
     @Override
     public void setGameID(String gameID) {
         this.gameID = gameID;
