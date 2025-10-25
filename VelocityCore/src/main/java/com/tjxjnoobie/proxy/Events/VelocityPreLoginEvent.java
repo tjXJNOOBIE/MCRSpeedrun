596c9012f28e14f5fbbf08da338f6bb5ade39b26
TJ <tjxjnoobie@gmail.com>
2025-10-24 21:17:16 -0700

    Refactor: Replace constructor injection with field injection for VelocityPreLoginEvent using @Inject annotations
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- VelocityCore/src/main/java/com/tjxjnoobie/proxy/Events/VelocityPreLoginEvent.java
+++ VelocityCore/src/main/java/com/tjxjnoobie/proxy/Events/VelocityPreLoginEvent.java
@@ -1,17 +1,16 @@
 package com.tjxjnoobie.proxy.Events;
 
 import com.tjxjnoobie.api.interfaces.IGlobalContext;
+import com.tjxjnoobie.api.platform.global.annotations.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
 
 import java.sql.SQLException;
 
 public class VelocityPreLoginEvent {
-    private final IGlobalContext globalContext;
+    @Inject private IGlobalContext globalContext;
 
-    public VelocityPreLoginEvent(IGlobalContext globalContext) {
-        this.globalContext = globalContext;
-    }
+
 
     @Subscribe
     public void onPreLogin(PreLoginEvent e) throws SQLException {
