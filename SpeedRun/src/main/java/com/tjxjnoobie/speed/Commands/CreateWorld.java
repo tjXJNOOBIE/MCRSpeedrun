ecb074c05118d151d9e727917d41b214940b0970
TJ <tjxjnoobie@gmail.com>
2025-10-25 04:12:46 -0700

    Refactor: Replace 'IUtils' implementation with 'IMCUtils'; Add Inject field for 'IWorldManager' in 'CreateWorld' command class; Replaced 'staffPrefix' with 'getMinecraftStaffInGamePrefix()' in 'CreateWorld'
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Commands/CreateWorld.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Commands/CreateWorld.java
@@ -1,8 +1,9 @@
 package com.tjxjnoobie.speed.Commands;
 
 import com.tjxjnoobie.api.interfaces.IGlobalContext;
-import com.tjxjnoobie.api.interfaces.IUtils;
+import com.tjxjnoobie.api.interfaces.IMCUtils;
 import com.tjxjnoobie.api.interfaces.IWorldManager;
+import com.tjxjnoobie.api.platform.global.annotations.Inject;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -10,32 +11,28 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CreateWorld implements CommandExecutor, IUtils {
+public class CreateWorld implements CommandExecutor, IMCUtils {
 
-    private final IGlobalContext globalContext;
-
-    public CreateWorld(IGlobalContext globalContext) {
-        this.globalContext = globalContext;
-    }
+    @Inject private IGlobalContext globalContext;
+    @Inject IWorldManager worldManager;
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        IWorldManager worldManager = globalContext.getWorldManager();
         Player player = (Player) commandSender;
         World.Environment environment;
 
         try {
             environment = World.Environment.valueOf(args[1]);
         } catch (IllegalArgumentException e) {
-            player.sendMessage(staffPrefix+"§fInvalid world type§e! Use §cNORMAL§f, §cNETHER§f,§c §for§c THE_END§f.");
+            player.sendMessage(getMinecraftStaffInGamePrefix()+"§fInvalid world type§e! Use §cNORMAL§f, §cNETHER§f,§c §for§c THE_END§f.");
             return true;
         }
         if(args.length != 2 && player.isOp()){
-            player.sendMessage(staffPrefix+ "Usage: /createworld <name> <type>");
+            player.sendMessage(getMinecraftStaffInGamePrefix()+ "Usage: /createworld <name> <type>");
 
         }else if(args.length == 2){
             String worldName = args[0];
-            player.sendMessage(staffPrefix+"Creating world §f" + args[1] + "§c with name §f" +args[0]);
+            player.sendMessage(getMinecraftStaffInGamePrefix()+"Creating world §f" + args[1] + "§c with name §f" +args[0]);
             worldManager.createWorld(worldName, environment);
             World world = Bukkit.getWorld(args[1]);
             player.teleport(world.getSpawnLocation());
