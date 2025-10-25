5599377ed91657d7df8ffce8ae7c1ba58a9bc1f4
TJ <tjxjnoobie@gmail.com>
2025-10-25 03:35:03 -0700

    Refractor: Replace `prefix` with `getMinecraftPrefix()` in `SpeedRunPlayerPickup`
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunPlayerPickup.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunPlayerPickup.java
@@ -45,7 +45,7 @@ import java.util.UUID;
         GameStateEnum currentState = gameState.getCurrentState();
         if(currentState != GameStateEnum.LOBBY) {
             if (item == Material.BLAZE_ROD && !HasBlazeRod && BlazeRodSize == 0 && entity instanceof Player) {
-                Bukkit.broadcastMessage(prefix + displayName + " has been the first to acquire a §cBlaze Rod!§f");
+                Bukkit.broadcastMessage(getMinecraftPrefix() + displayName + " has been the first to acquire a §cBlaze Rod!§f");
                 gameManager.addBlazeRod(uuid, name);
                 mcUtils.playSoundForAll(allLocation, Sound.ENTITY_BLAZE_DEATH, 1.0f, 1.0f);
             } else if (HasBlazeRod) {
@@ -53,15 +53,15 @@ import java.util.UUID;
             }
 
             if (item == Material.ENDER_EYE && !HasEye && EyeSize == 0 && entity instanceof Player) {
-                Bukkit.broadcastMessage(prefix + name + " has been the first to acquire a §cEye of Ender!§f");
+                Bukkit.broadcastMessage(getMinecraftPrefix() + name + " has been the first to acquire a §cEye of Ender!§f");
                 gameManager.addEnderEye(uuid, name);
                 mcUtils.playSoundForAll(allLocation, Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f);
             } else if (HasEye) {
-                mcUtils.sendMessageToAll(prefix+displayName+" has acquired a Eye of Ender!");
+                mcUtils.sendMessageToAll(getMinecraftPrefix()+displayName+" has acquired a Eye of Ender!");
                 return;
             }
             if (item == Material.ENDER_PEARL && !HasPearl && PearlSize == 0 && entity instanceof Player) {
-                Bukkit.broadcastMessage(prefix + name + " has been the first to acquire a §cEnder Pearl!§f");
+                Bukkit.broadcastMessage(getMinecraftPrefix() + name + " has been the first to acquire a §cEnder Pearl!§f");
                 gameManager.addEnderEye(uuid, name);
                 mcUtils.playSoundForAll(allLocation, Sound.ENTITY_ENDERMAN_DEATH, 1.0f, 1.0f);
             } else if (HasPearl) {
