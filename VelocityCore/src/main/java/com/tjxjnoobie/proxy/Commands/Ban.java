32f9d0a73bee41a4d27a04b065dbb5fc02191ae9
TJ <tjxjnoobie@gmail.com>
2025-10-24 21:10:39 -0700

    Refactor: Replace constructor injection with field injection for Ban command using @Inject annotations
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- VelocityCore/src/main/java/com/tjxjnoobie/proxy/Commands/Ban.java
+++ VelocityCore/src/main/java/com/tjxjnoobie/proxy/Commands/Ban.java
@@ -1,6 +1,7 @@
 package com.tjxjnoobie.proxy.Commands;
 
 import com.tjxjnoobie.api.interfaces.*;
+import com.tjxjnoobie.api.platform.global.annotations.Inject;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.ConsoleCommandSource;
@@ -21,13 +22,10 @@ import java.util.concurrent.CompletableFuture;
 
 public class Ban implements SimpleCommand, IUtils {
 
-    private final IGlobalContext globalContext;
-    private final ProxyServer proxyServer;
+    @Inject private IGlobalContext globalContext;
+    @com.google.inject.Inject private ProxyServer proxyServer;
 
-    public Ban(IGlobalContext globalContext, ProxyServer proxyServer) {
-        this.globalContext = globalContext;
-        this.proxyServer = proxyServer;
-    }
+
     /**
      * Executes the ban command, which bans a player from the server.
      *
