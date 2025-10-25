a6c46ec5fecf00c27d42c4f331ded45ead919786
TJ <tjxjnoobie@gmail.com>
2025-10-25 03:02:14 -0700

    Refactor: Update `IGlobalContext` to use generic `IUtils` and `IWorldManager` instead of type-parametrized versions
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- API/src/main/java/com/tjxjnoobie/api/interfaces/IGlobalContext.java
+++ API/src/main/java/com/tjxjnoobie/api/interfaces/IGlobalContext.java
@@ -22,9 +22,9 @@ public interface IGlobalContext extends IAbstractClassMetaData<IGlobalContext> {
     // Core getters using interfaces
     IGameMode getGameMode();
     IGameState getGameState();
-    IUtils<IGlobalContext> getUtils();
+    IUtils getUtils();
     ISpeedrunStatsCache getSpeedrunStatsCache();
-    IWorldManager<ISpeedRunContext> getWorldManager();
+    IWorldManager getWorldManager();
     IRatingCache getRatingCache();
     IRating getRating();
     IRatingAPI getRatingAPI();
@@ -55,10 +55,10 @@ public interface IGlobalContext extends IAbstractClassMetaData<IGlobalContext> {
     IGlobalContext setGameMode(IGameMode gameMode);
     IGlobalContext setGameState(IGameState gameState);
 
-    IGlobalContext setUtils(IUtils<IGlobalContext> utils);
+    IGlobalContext setUtils(IUtils utils);
     IGlobalContext setStatsCache(ISpeedrunStatsCache statsCache);
 
-    IGlobalContext setWorldManager(IWorldManager<ISpeedRunContext> worldManager);
+    IGlobalContext setWorldManager(IWorldManager worldManager);
     IGlobalContext setRatingCache(IRatingCache ratingCache);
     IGlobalContext setRating(IRating rating);
     IGlobalContext setRatingAPI(IRatingAPI ratingAPI);
