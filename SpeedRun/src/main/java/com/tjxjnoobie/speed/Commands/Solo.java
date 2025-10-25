b8caf28d7f4df1f7c67a9c126ddcb00aeea7144e
TJ <tjxjnoobie@gmail.com>
2025-10-25 03:20:19 -0700

    Refractor: Switched 'Utils' dependency implementation to 'IMCUtils' and added ''@Inject' annotation for 'GameManager'; Removed 'GameManager' class constructor
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Commands/Solo.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Commands/Solo.java
@@ -1,6 +1,7 @@
 package com.tjxjnoobie.speed.Commands;
 
-import com.tjxjnoobie.api.internal.utils.Utils;
+import com.tjxjnoobie.api.interfaces.IMCUtils;
+import com.tjxjnoobie.api.platform.global.annotations.Inject;
 import com.tjxjnoobie.speed.managers.GameManager;
 import org.bukkit.Location;
 import org.bukkit.Sound;
@@ -12,13 +13,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
 
-public class Solo implements CommandExecutor {
+public class Solo implements CommandExecutor, IMCUtils {
 
-    @Inject private GameManager gameManager; 
-    public Solo(Utils utils, GameManager gameManager) {
-        this.utils = utils;
-        this.gameManager = gameManager;
-    }
+    @Inject private GameManager gameManager;
+
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -27,9 +25,9 @@ public class Solo implements CommandExecutor {
         Location location = player.getLocation();
         boolean canSolo = gameManager.canSolo();
         if(length > 0){
-            player.sendMessage(utils.prefix+"Usage: /solo");
+            player.sendMessage(getMinecraftPrefix()+"Usage: /solo");
         }else if(canSolo){
-            player.sendMessage(utils.prefix+"Starting game solo");
+            player.sendMessage(getMinecraftPrefix()+"Starting game solo");
             player.playSound(location, Sound.BLOCK_NOTE_BLOCK_BELL, 1.0f,1.0f);
             try {
                 gameManager.startGame();
@@ -37,7 +35,7 @@ public class Solo implements CommandExecutor {
                 throw new RuntimeException(e);
             }
         }else{
-            player.sendMessage(utils.prefix+"Can't start game solo!");
+            player.sendMessage(getMinecraftPrefix()+"Can't start game solo!");
         }
 
         return false;
