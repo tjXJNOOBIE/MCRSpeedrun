d950a64b8c0e069fd0f5d368386a2d6f6f9b8b55
TJ <tjxjnoobie@gmail.com>
2025-11-01 18:46:32 -0700

    Added: Create `DependencyClass` skeleton to further type safe Class<?/T> for our DI system.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

---
+++ API/src/main/java/com/tjxjnoobie/api/dependency/metadata/DependencyClass.java
@@ -0,0 +1,29 @@
+/*
+ * TJVD License (TJ Valentine’s Discretionary License) — Version 1.0 (2025)
+ *
+ * Copyright (c) 2025 Taheesh Valentine
+ *
+ * This source code is protected under the TJVD License.
+ * SEE LICENSE.TXT
+ */
+
+package com.tjxjnoobie.api.dependency.metadata;
+
+import java.lang.annotation.Annotation;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * DependencyClass – TODO: implement class functionality
+ * Auto-generated skeleton by MondayGPT-style template
+ *
+ * @author TJ
+ * @since 11/1/2025
+ */
+public class DependencyClass<T> {
+
+    private Class<T> dependencyClass;
+    private UUID dependencyId;
+    private Date creationTime;
+
+}
