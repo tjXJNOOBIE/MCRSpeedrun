164960157fbab1dc11f9325c2f1fff89e177ce58
TJ <tjxjnoobie@gmail.com>
2025-11-04 01:12:06 -0800

    Refactor: Add default method implementations to `IPlayerProfile` interface to simplify usage and provide base functionality.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- API/src/main/java/com/tjxjnoobie/api/interfaces/IPlayerProfile.java
+++ API/src/main/java/com/tjxjnoobie/api/interfaces/IPlayerProfile.java
@@ -7,18 +7,23 @@ import java.util.UUID;
  * Interface for player profile management
  */
 public interface IPlayerProfile {
-    
+
 
     /**
      * Creates a new player profile
-     * @param playerId The player's UUID
+     *
+     * @param playerId   The player's UUID
      * @param playerName The player's name
      */
-    void createProfile(UUID playerId, String playerName) throws SQLException;
-    
+    default void createProfile(UUID playerId, String playerName) throws SQLException {
+    }
 
 
-    String getUUIDFromUsername(String punishTable, String targetName, String punishTable1) throws SQLException;
+    default String getUUIDFromUsername(String punishTable, String targetName, String punishTable1) throws SQLException {
+        return "";
+    }
 
-    boolean playerExistsFromUsername(String targetName, String punish, String punish1) throws SQLException;
+    default boolean playerExistsFromUsername(String targetName, String punish, String punish1) throws SQLException {
+        return false;
+    }
 }
