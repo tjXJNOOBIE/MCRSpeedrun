fcef6ce93ed7de1f85e0ac1c24306388c710008c
TJ <tjxjnoobie@gmail.com>
2025-11-07 02:37:06 -0800

    Added: `RegistryData` class skeleton with generic types for registry type and data, implements `IRegistryData` and `IAbstractRegistry`.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

---
+++ API/src/main/java/com/tjxjnoobie/api/platform/global/registry/metadata/RegistryData.java
@@ -0,0 +1,29 @@
+/*
+ * TJVD License (TJ Valentine’s Discretionary License) — Version 1.0 (2025)
+ *
+ * Copyright (c) 2025 Taheesh Valentine
+ *
+ * This source code is protected under the TJVD License.
+ * SEE LICENSE.TXT
+ */
+
+package com.tjxjnoobie.api.platform.global.registry.metadata;
+
+import com.tjxjnoobie.api.platform.global.registry.IAbstractRegistry;
+import com.tjxjnoobie.api.platform.global.registry.enums.RegistryType;
+import com.tjxjnoobie.api.platform.global.registry.metadata.interfaces.IRegistryData;
+
+/**
+ * RegistryData – TODO: implement class functionality
+ * Auto-generated skeleton by MondayGPT-style template
+ *
+ * @author TJ
+ * @since 11/5/2025
+ */
+public class RegistryData<REG_TYPE extends RegistryType<REG_TYPE,REG_DATA>,
+        REG_DATA extends IRegistryData<REG_TYPE,REG_DATA>>
+        implements IRegistryData<REG_TYPE,REG_DATA>, IAbstractRegistry<REG_TYPE, REG_DATA> {
+
+
+
+}
