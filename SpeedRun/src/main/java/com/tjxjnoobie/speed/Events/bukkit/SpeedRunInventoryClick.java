e232abd57df08acc269f06237376d700fc941bb6
TJ <tjxjnoobie@gmail.com>
2025-11-07 03:30:22 -0800

    Refactor: Replace `ISpeedRunContext` dependency in `SpeedRunInventoryClick` and `SpeedRunInventoryDrag` with direct interface implementations to simplify logic and reduce coupling.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunInventoryClick.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunInventoryClick.java
@@ -1,9 +1,7 @@
 package com.tjxjnoobie.speed.Events.bukkit;
 
 import com.tjxjnoobie.api.interfaces.IMCUtils;
-import com.tjxjnoobie.api.interfaces.ISpeedRunContext;
 import com.tjxjnoobie.api.interfaces.IVoting;
-import com.tjxjnoobie.api.platform.global.annotations.Inject;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,18 +12,17 @@ import org.bukkit.inventory.ItemStack;
 import java.sql.SQLException;
 import java.util.UUID;
 
-public class SpeedRunInventoryClick implements Listener, IMCUtils {
+public class SpeedRunInventoryClick implements Listener, IMCUtils, IVoting {
 
-    @Inject private ISpeedRunContext speedRunContext;
 
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) throws SQLException {
-        IVoting voting = speedRunContext.getVoting();
+        
         Player player = (Player) e.getWhoClicked();
         UUID uuid = player.getUniqueId();
 
-        boolean hasVoted = voting.getHasVotedHash().contains(uuid);
+        boolean hasVoted = getHasVotedHash().contains(uuid);
         if (e.getView().getTitle().equals("§aVote for a Gamemode") && e.getClickedInventory() != null) {
             if(hasVoted){
                 player.sendMessage(getMinecraftPrefix()+"§cYou have already voted!");
@@ -39,9 +36,9 @@ public class SpeedRunInventoryClick implements Listener, IMCUtils {
             ItemStack clickedItem = e.getCurrentItem();
             if (clickedItem != null && clickedItem.getType() == Material.PAPER && clickedItem.getItemMeta() != null) {
                 String gamemode = clickedItem.getItemMeta().getDisplayName().replace("§e", "");
-                voting.vote(player,gamemode);
+                vote(player,gamemode);
                 player.sendMessage(getMinecraftPrefix()+"§aYou voted for " + gamemode + "!");
-                voting.getHasVotedHash().add(uuid);
+                getHasVotedHash().add(uuid);
 
                 player.closeInventory();
             }
--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunInventoryDrag.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunInventoryDrag.java
@@ -2,24 +2,16 @@ package com.tjxjnoobie.speed.Events.bukkit;
 
 import com.tjxjnoobie.api.enums.GameStateEnum;
 import com.tjxjnoobie.api.interfaces.IGameState;
-import com.tjxjnoobie.api.interfaces.ISpeedRunContext;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryDragEvent;
 
-public class SpeedRunInventoryDrag implements Listener {
-
-    private final ISpeedRunContext speedRunContext;
-
-    public SpeedRunInventoryDrag(ISpeedRunContext speedRunContext) {
-        this.speedRunContext = speedRunContext;
-    }
+public class SpeedRunInventoryDrag implements Listener, IGameState {
 
 
     @EventHandler
     public void onDrag(InventoryDragEvent e) {
-        IGameState gameState = speedRunContext.getGameState();
-        GameStateEnum currentState = gameState.getCurrentState();
+        GameStateEnum currentState = getCurrentState();
         if(currentState == GameStateEnum.LOBBY || currentState == GameStateEnum.ENDING){
             e.setCancelled(true);
         }
