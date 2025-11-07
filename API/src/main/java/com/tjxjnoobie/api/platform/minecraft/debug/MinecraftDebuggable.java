9d9feb15762da1e7eb1baf6d4515620df15dac0a
TJ <tjxjnoobie@gmail.com>
2025-11-07 02:49:02 -0800

    Refactor: Remove `ISpeedRunContext` dependency from `MinecraftDebuggable` to simplify class design.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- API/src/main/java/com/tjxjnoobie/api/platform/minecraft/debug/MinecraftDebuggable.java
+++ API/src/main/java/com/tjxjnoobie/api/platform/minecraft/debug/MinecraftDebuggable.java
@@ -2,19 +2,10 @@ package com.tjxjnoobie.api.platform.minecraft.debug;
 
 import com.tjxjnoobie.api.interfaces.IDebugger;
 import com.tjxjnoobie.api.interfaces.IMCUtils;
-import com.tjxjnoobie.api.interfaces.ISpeedRunContext;
-import com.tjxjnoobie.api.platform.global.annotations.Inject;
 import org.bukkit.entity.Player;
 
 public class MinecraftDebuggable implements IDebugger, IMCUtils {
 
-    @Inject
-    private ISpeedRunContext speedRunContext;
-
-    @Override
-    public ISpeedRunContext getSpeedRunContext() {
-        return speedRunContext;
-    }
 
     @Override
     public void sendDebugMessage(Player player, String message) {
