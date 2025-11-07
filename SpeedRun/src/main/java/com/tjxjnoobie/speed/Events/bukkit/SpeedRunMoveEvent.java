e63a54bb476c8b86c18749f9ed29acfe99aad5d1
TJ <tjxjnoobie@gmail.com>
2025-11-07 03:37:40 -0800

    Refactor: Replace `SpeedRunContext` dependency in `SpeedRunMoveEvent` with `IGameManager` to reduce coupling and simplify logic.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunMoveEvent.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunMoveEvent.java
@@ -2,8 +2,6 @@ package com.tjxjnoobie.speed.Events.bukkit;
 
 import com.tjxjnoobie.api.enums.GameStateEnum;
 import com.tjxjnoobie.api.interfaces.IGameManager;
-import com.tjxjnoobie.api.interfaces.IGameState;
-import com.tjxjnoobie.speed.managers.SpeedRunContext;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,19 +9,13 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.UUID;
 
-public class SpeedRunMoveEvent implements Listener {
+public class SpeedRunMoveEvent implements Listener, IGameManager {
 
-    private final SpeedRunContext speedRunContext;
-
-    public SpeedRunMoveEvent(SpeedRunContext speedRunContext) {
-        this.speedRunContext = speedRunContext;
-    }
 
 
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
-        IGameState gameState = speedRunContext.getGameState();
-        IGameManager gameManager = speedRunContext.getGameManager();
+
         Player player = e.getPlayer();
         UUID uuid = player.getUniqueId();
         double toX = e.getTo().getX();
@@ -32,9 +24,9 @@ public class SpeedRunMoveEvent implements Listener {
         double fromX = e.getFrom().getX();
         double fromY = e.getFrom().getY();
         double fromZ = e.getFrom().getZ();
-        boolean canMove = gameManager.canMove();
-        boolean InGamePlayer = gameManager.getPlaying().containsKey(uuid);
-        GameStateEnum currentState = gameState.getCurrentState();
+        boolean canMove = canMove();
+        boolean InGamePlayer = getPlaying().containsKey(uuid);
+        GameStateEnum currentState = getCurrentState();
         if(currentState == GameStateEnum.PREGAME){
 
             if(toX != fromX || toY != fromY||
