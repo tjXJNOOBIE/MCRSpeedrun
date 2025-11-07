1490cbd86b6828354da09a6dac10f68b13e6003a
TJ <tjxjnoobie@gmail.com>
2025-11-07 07:59:52 -0800

    Removed: Extensive unused wrapper methods in 'DependencyClass', previously wrapping 'java.lang.Class' methods such as 'getMethods', 'getFields', and annotation retrieval methods. These were redundant and cluttered the class design. Simplified code for better clarity and maintainability.
    Refactor: Replaced private field 'IDependencyClass<CLASS>' with 'CLASS' in 'DependencyClass' for a more precise and direct representation of the dependency.
    
    Added: Introduced new public field 'dependencyId' and 'creationTime' to hold core metadata. Added a constructor to initialize 'interfaceClass'.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- API/src/main/java/com/tjxjnoobie/api/dependency/metadata/DependencyClass.java
+++ API/src/main/java/com/tjxjnoobie/api/dependency/metadata/DependencyClass.java
@@ -10,14 +10,9 @@
 package com.tjxjnoobie.api.dependency.metadata;
 
 import com.tjxjnoobie.api.dependency.metadata.interfaces.IDependencyClass;
+import com.tjxjnoobie.api.dependency.metadata.interfaces.IDependencyMetaData;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 import java.util.Date;
-import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -27,34 +22,41 @@ import java.util.UUID;
  * @author TJ
  * @since 11/1/2025
  */
-public class DependencyClass<CLASS extends IDependencyClass<?>> implements IDependencyClass<CLASS> {
+public class DependencyClass<CLASS extends IDependencyClass<?>>
+        implements IDependencyClass<CLASS> {
 
     //To add to class Doc sstring: We have to wrap Class methods here since Class object is final at runtime and not easily changeable
     //TODO: Add logging to all methods so we can see what methods are being called and their parameters
     // This can also be used to make it so we can make a registry of all classes and their methods, fields, extends, implmentations, etc.
-    private IDependencyClass<CLASS> dependencyClass;
-
-    private UUID dependencyId;
-    private Date creationTime;
-
-
+    private CLASS dependencyClass;
+    private IDependencyMetaData<CLASS, ?> metadata;
+    private final Class<?> interfaceClass;
+    public UUID dependencyId;
+    public Date creationTime;
+    public DependencyClass(Class<?> interfaceClass) {
+        this.interfaceClass = interfaceClass;
+    }
     @Override
-    public IDependencyClass<CLASS> getDependencyClass() {
+    public CLASS getDependencyClass(){
         return dependencyClass;
     }
     @Override
-    public UUID getDependencyId() {
-        return dependencyId;
+    public Class<?> getDependencyRawInterface() {
+        return interfaceClass;
     }
-
     @Override
     public Date getCreationTime() {
         return creationTime;
     }
 
     @Override
-    public void setDependencyClass(IDependencyClass<CLASS> dependencyClass) {
-        this.dependencyClass = dependencyClass;
+    public UUID getDependencyId() {
+        return dependencyId;
+    }
+
+    @Override
+    public void setDependencyClass(CLASS dependencyClass) {
+        this.dependencyClass = (CLASS) dependencyClass;
     }
 
     @Override
@@ -70,192 +72,6 @@ public class DependencyClass<CLASS extends IDependencyClass<?>> implements IDepe
 
     }
 
-    // =====================================================
-    // Wrapped java.lang.Class methods
-    // =====================================================
 
-    @Override
-    public String getName() {
-        return getDependencyClass().getName();
-    }
-
-    @Override
-    public String getSimpleName() {
-        return getDependencyClass().getSimpleName();
-    }
-
-    @Override
-    public String getPackageName() {
-        return getDependencyClass().getClass().getPackageName();
-    }
-
-    @Override
-    public boolean isInterface() {
-        return getDependencyClass().isInterface();
-    }
-
-    @Override
-    public boolean isEnum() {
-        return getDependencyClass().isEnum();
-    }
-
-    @Override
-    public boolean isAnnotation() {
-        return getDependencyClass().getClass().isAnnotation();
-    }
-
-    @Override
-    public boolean isPrimitive() {
-        return getDependencyClass().getClass().isPrimitive();
-    }
-
-    @Override
-    public boolean isArray() {
-        return getDependencyClass().getClass().isArray();
-    }
-
-    @Override
-    public boolean isAssignableFrom(Class<?> cls) {
-        return getDependencyClass().getClass().isAssignableFrom(cls);
-    }
-
-    @Override
-    public boolean isInstance(Object obj) {
-        return getDependencyClass().getClass().isInstance(obj);
-    }
-
-    @Override
-    public Class<?> getSuperclass() {
-        return getDependencyClass().getClass().getSuperclass();
-    }
-
-    @Override
-    public Class<?>[] getInterfaces() {
-        return getDependencyClass().getClass().getInterfaces();
-    }
-
-    @Override
-    public Type[] getGenericInterfaces() {
-        return getDependencyClass().getClass().getGenericInterfaces();
-    }
-
-    @Override
-    public Type getGenericSuperclass() {
-        return getDependencyClass().getClass().getGenericSuperclass();
-    }
-
-    @Override
-    public Constructor<?>[] getConstructors() {
-        return getDependencyClass().getClass().getConstructors();
-    }
-
-    @Override
-    public Constructor<?> getConstructor(Class<?>... parameterTypes)
-            throws NoSuchMethodException {
-        return getDependencyClass().getClass().getConstructor(parameterTypes);
-    }
-
-    @Override
-    public Constructor<?>[] getDeclaredConstructors() {
-        return getDependencyClass().getClass().getDeclaredConstructors();
-    }
-
-    @Override
-    public Constructor<?> getDeclaredConstructor(Class<?>... parameterTypes)
-            throws NoSuchMethodException {
-        return getDependencyClass().getClass().getDeclaredConstructor(parameterTypes);
-    }
-
-    @Override
-    public Field[] getFields() {
-        return getDependencyClass().getClass().getFields();
-    }
-
-    @Override
-    public Field getField(String name) throws NoSuchFieldException {
-        return getDependencyClass().getClass().getField(name);
-    }
-
-    @Override
-    public Field[] getDeclaredFields() {
-        return getDependencyClass().getClass().getDeclaredFields();
-    }
-
-    @Override
-    public Field getDeclaredField(String name) throws NoSuchFieldException {
-        return getDependencyClass().getClass().getDeclaredField(name);
-    }
-
-    @Override
-    public Method[] getMethods() {
-        return getDependencyClass().getClass().getMethods();
-    }
-
-    @Override
-    public Method getMethod(String name, Class<?>... parameterTypes)
-            throws NoSuchMethodException {
-        return getDependencyClass().getClass().getMethod(name, parameterTypes);
-    }
-
-    @Override
-    public Method[] getDeclaredMethods() {
-        return getDependencyClass().getClass().getDeclaredMethods();
-    }
-
-    @Override
-    public Method getDeclaredMethod(String name, Class<?>... parameterTypes)
-            throws NoSuchMethodException {
-        return getDependencyClass().getClass().getDeclaredMethod(name, parameterTypes);
-    }
-
-    @Override
-    public Annotation[] getAnnotations() {
-        return getDependencyClass().getClass().getAnnotations();
-    }
-
-    @Override
-    public <A extends Annotation> A getAnnotation(Class<A> annotationClass) {
-        return getDependencyClass().getClass().getAnnotation(annotationClass);
-    }
-
-    @Override
-    public Annotation[] getDeclaredAnnotations() {
-        return getDependencyClass().getClass().getDeclaredAnnotations();
-    }
-
-    @Override
-    public <A extends Annotation> A[] getAnnotationsByType(Class<A> annotationClass) {
-        return getDependencyClass().getClass().getAnnotationsByType(annotationClass);
-    }
-
-    @Override
-    public <A extends Annotation> A getDeclaredAnnotation(Class<A> annotationClass) {
-        return getDependencyClass().getClass().getDeclaredAnnotation(annotationClass);
-    }
-
-    // =====================================================
-    // Utility
-    // =====================================================
-
-    @Override
-    public String toString() {
-        return "DependencyClass[" +
-                "dep=" + (dependencyClass != null ? dependencyClass.getName() : "null") +
-                ", id=" + dependencyId +
-                ", created=" + creationTime +
-                ']';
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(dependencyClass, dependencyId);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof DependencyClass<?> other)) return false;
-        return Objects.equals(this.dependencyClass, other.dependencyClass)
-                && Objects.equals(this.dependencyId, other.dependencyId);
-    }
 }
 
