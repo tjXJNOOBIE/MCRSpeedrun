45d5f73e9f21983375539d8764f9829f87dce726
TJ <tjxjnoobie@gmail.com>
2025-11-07 03:28:41 -0800

    Refactor: Replace `ISpeedRunContext` dependency in `SpeedRunHungerLevelChange` with direct interface implementation to simplify logic and reduce coupling.
    Signed-off-by: TJ <tjxjnoobie@gmail.com>

--- SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunHungerLevelChange.java
+++ SpeedRun/src/main/java/com/tjxjnoobie/speed/Events/bukkit/SpeedRunHungerLevelChange.java
@@ -2,25 +2,18 @@ package com.tjxjnoobie.speed.Events.bukkit;
 
 import com.tjxjnoobie.api.enums.GameStateEnum;
 import com.tjxjnoobie.api.interfaces.IGameState;
-import com.tjxjnoobie.api.interfaces.ISpeedRunContext;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
-public class SpeedRunHungerLevelChange implements Listener {
+public class SpeedRunHungerLevelChange implements Listener, IGameState {
 
-    private final ISpeedRunContext speedRunContext;
-
-    public SpeedRunHungerLevelChange(ISpeedRunContext speedRunContext) {
-        this.speedRunContext = speedRunContext;
-    }
 
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
-        IGameState gameState = speedRunContext.getGameState();
         if (event.getEntity() instanceof Player) {
-            GameStateEnum currentState = gameState.getCurrentState();
+            GameStateEnum currentState = getCurrentState();
             if (currentState == GameStateEnum.LOBBY || currentState == GameStateEnum.PREGAME || currentState == GameStateEnum.ENDING) {
                 event.setCancelled(true);
             }
