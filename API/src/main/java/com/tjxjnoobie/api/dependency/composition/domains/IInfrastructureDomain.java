dbc8e3fb4ddac9681d454ead79173e5166a4ec25
TJ <tjxjnoobie@gmail.com>
2026-04-06 03:26:04 -0700

    Extract tavall-di module

--- API/src/main/java/com/tjxjnoobie/api/dependency/composition/domains/IInfrastructureDomain.java
+++
@@ -1,9 +0,0 @@
-package com.tjxjnoobie.api.dependency.composition.domains;
-
-import com.tjxjnoobie.api.dependency.IDependencyInjectableInterface;
-
-/**
- * Domain bridge for infrastructure dependencies composed by the Novus composition generator.
- */
-public interface IInfrastructureDomain extends IInfrastructureDomainGenerated, IDependencyInjectableInterface {
-}
